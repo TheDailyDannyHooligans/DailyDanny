b866d8d4e7cbe1a62851a2bc32fc3bcc2a8efccd
Michael-Parker719 <scoutmodelmp@gmail.com>
2024-04-11 17:13:28 -0400

    added Current article

--- DailyDanny/src/routes/account.js
+++ DailyDanny/src/routes/account.js
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
+export const currentArticle = writable(null);
 export const account = writable(null);
 export const isAdmin = writable(false);
 export const isEditor = writable(false);
