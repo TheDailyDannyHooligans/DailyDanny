751268c0d6aef18db21a508aaa13bd90dfc93b69
Andrew Valentin <95504743+andrew-valentin@users.noreply.github.com>
2024-04-18 14:13:35 -0400

    added thumbnail

--- api/models/articlemodel.js
+++ api/models/articlemodel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 let articleSchema = new mongoose.Schema({
+    thumbnailid: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
